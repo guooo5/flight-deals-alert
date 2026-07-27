fba21799ace99edb8d52f86c3d79bd0bb1a16cc2
guooo5 <guo.chen.1@stonybrook.edu>
2026-07-27 19:42:23 -0400

    feat: created FlightSearch class to interact with serp flight search api

--- flight_search.py
+++ flight_search.py
@@ -1,3 +1,37 @@
+import os 
+from dotenv import load_dotenv
+import requests
+load_dotenv()
+
+SERPAPI_ENDPOINT = "https://serpapi.com/search"
+
 class FlightSearch:
     #This class for talking to the Flight Search API
-    pass
+    
+    def __init__(self):
+        self._api_key = os.environ["SERPAPI_API_KEY"]
+    
+    def check_flights(self, origin_city_code, dest_city_code, from_time, to_time):
+        params = {
+            "engine": "google_flights",
+            "departure_id": origin_city_code,
+            "arrival_id": dest_city_code,
+            "outbound_date": from_time,
+            "return_date": to_time,
+            "type": "1",
+            "adults":"1",
+            "currency": "USD",
+            "api_key": self._api_key
+        }
+        
+        response = requests.get(url=SERPAPI_ENDPOINT, params=params)
+        
+        if response.status_code != 200:
+            print(f"check_flight() response code: {response.status_code}")
+            return None 
+        
+        data = response.json()
+        if "error" in data:
+            print(f"API error: {data['error']}")
+            return None 
+        return data 
--- main.py
+++ main.py
@@ -1,7 +1,39 @@
+import requests_cache #save API responses locally 
 from pprint import pprint 
+from datetime import datetime, timedelta
 
 from data_manager import DataManager
+from flight_search import FlightSearch
+
+#------conserve requests 
+requests_cache.install_cache(
+    "flight_cache",
+    url_expire_after={
+        #cache everything (except sheety) for 1hour 
+        "*.sheety.co*": requests_cache.DO_NOT_CACHE,
+        "*":3600,
+    }
+)
+
+#----talking to sheety 
 data = DataManager()
 sheet_data = data.get_data()
-
 pprint(sheet_data)
+
+
+#----set the dates 
+tomorrow = datetime.now() + timedelta(days=1)
+six_month_from_today = datetime.now() + timedelta(days=(6 * 30))
+
+
+#-----do flight search 
+flight_search = FlightSearch()
+
+flights = flight_search.check_flights(
+    origin_city_code='LHR',
+    dest_city_code='CDG',
+    from_time=tomorrow,
+    to_time=six_month_from_today
+)
+
+pprint(flights)
